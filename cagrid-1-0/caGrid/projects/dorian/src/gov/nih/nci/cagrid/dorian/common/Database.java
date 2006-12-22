bdd15e0761fc282258e66c92da93c9cd439fdc15
langella <langella>
2006-12-22 23:46:15 +0000

    *** empty log message ***

--- cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/common/Database.java
+++ cagrid-1-0/caGrid/projects/dorian/src/gov/nih/nci/cagrid/dorian/common/Database.java
@@ -17,7 +17,7 @@ import org.projectmobius.db.Query;
  * @author <A HREF="MAILTO:langella@bmi.osu.edu">Stephen Langella </A>
  * @author <A HREF="MAILTO:oster@bmi.osu.edu">Scott Oster </A>
  * @author <A HREF="MAILTO:hastings@bmi.osu.edu">Shannon Hastings </A>
- * @version $Id: Database.java,v 1.17.2.1 2006-12-22 21:53:36 langella Exp $
+ * @version $Id: Database.java,v 1.17.2.2 2006-12-22 23:45:55 langella Exp $
  */
 public class Database extends LoggingObject {
 
@@ -39,7 +39,7 @@ public class Database extends LoggingObject {
 		try {
 			if (!dbBuilt) {
 				if (!databaseExists(database)) {
-					Query.update(this.root, "create database " + database+" COLLATE ascii_bin");
+					Query.update(this.root, "create database " + database);
 				}
 				if (dorian == null) {
 					dorian = new ConnectionManager(database, root
