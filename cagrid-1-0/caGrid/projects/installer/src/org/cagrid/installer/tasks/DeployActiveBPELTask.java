68e17670e4f47429fecc2d9306bf447463996a44
parmarv <parmarv>
2007-07-12 14:43:17 +0000

    *** empty log message ***

--- cagrid-1-0/caGrid/projects/installer/src/org/cagrid/installer/tasks/DeployActiveBPELTask.java
+++ cagrid-1-0/caGrid/projects/installer/src/org/cagrid/installer/tasks/DeployActiveBPELTask.java
@@ -5,7 +5,10 @@ package org.cagrid.installer.tasks;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -20,18 +23,41 @@ public class DeployActiveBPELTask extends BasicTask {
 
 	private static final Log logger = LogFactory.getLog(DeployActiveBPELTask.class);
 	
-	
+	private Map<String, String> environment;
 
 	
 
 	public DeployActiveBPELTask(String name, String description) {
 		super(name, description);
+		this.environment = new HashMap<String,String>();
 		
 	}
 
 	protected Object internalExecute(Map state) throws Exception {
 		
+		
+		
+		Map<String, String> env = new HashMap<String, String>(System
+				.getenv());
+		for (Iterator i = this.environment.entrySet().iterator(); i
+				.hasNext();) {
+			Entry entry = (Entry) i.next();
+			if (entry.getKey() instanceof String
+					&& entry.getValue() instanceof String) {
+				env.put((String) entry.getKey(), (String) entry.getValue());
+			}
+		}
+		env.put((String)"GLOBUS_LOCATION", (String)state.get(Constants.GLOBUS_HOME));
+		env.put((String)"CATALINA_HOME", (String)state.get(Constants.TOMCAT_HOME));
+		Map<String,String> myEnv = new HashMap<String,String>(env);
+	
+		String[] envp = new String[myEnv.size()];
+		int i = 0;
+		for (String key : myEnv.keySet()) {
+			envp[i++] = key + "=" + myEnv.get(key);
+		}
 
+						
 		
 		String baseDir = InstallerUtils.getRequiredProperty(state,Constants.ACTIVEBPEL_HOME);
 		try {
@@ -43,7 +69,7 @@ public class DeployActiveBPELTask extends BasicTask {
 				throw new RuntimeException("ActiveBPEL directory doesnt exist."
 						+ baseDir);
 			}
-			runCommand(activebpelHome);
+			runCommand(activebpelHome,envp);
 			
 		} catch (Exception ex) {
 			throw new RuntimeException("Error encountered: " + ex.getMessage(),
@@ -52,7 +78,7 @@ public class DeployActiveBPELTask extends BasicTask {
 		return null;
 	}
 
-	protected void runCommand(File dir) throws IOException, InterruptedException {
+	protected void runCommand(File dir, String[] envp) throws IOException, InterruptedException {
 
 
 		boolean isWindows = false;
@@ -61,7 +87,6 @@ public class DeployActiveBPELTask extends BasicTask {
 		}
 		//build command
 		String[] cmd = new String[4];
-		//
 		
 		if(isWindows){
 			cmd[0]="cmd.exe" ;
@@ -77,7 +102,7 @@ public class DeployActiveBPELTask extends BasicTask {
 		logger.debug("########## Executing: " + cmd);
 
 		// run ant
-		Process p = Runtime.getRuntime().exec(cmd, null,dir);
+		Process p = Runtime.getRuntime().exec(cmd, envp,dir);
 		// track stdout and stderr
 		StringBuffer stdout = new StringBuffer();
 		StringBuffer stderr = new StringBuffer();
