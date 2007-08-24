dfba53b3f84c26897cb2b3578b46b13b3dd08d40
hastings <hastings>
2007-08-24 00:04:18 +0000

    adding dialog to choose speciic restore point

--- cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/extension/utils/ExtensionUtilities.java
+++ cagrid-1-0/caGrid/projects/introduce/src/java/Introduce/gov/nih/nci/cagrid/introduce/extension/utils/ExtensionUtilities.java
@@ -24,7 +24,7 @@ import org.projectmobius.common.XMLUtilities;
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
  * 
  * @created Jun 7, 2006 
- * @version $Id: ExtensionUtilities.java,v 1.5.6.4 2007-08-13 18:40:13 hastings Exp $ 
+ * @version $Id: ExtensionUtilities.java,v 1.5.6.5 2007-08-24 00:04:18 hastings Exp $ 
  */
 public class ExtensionUtilities {
 	public static final String CLASSPATHENTRY_ELEMENT = "classpathentry";
