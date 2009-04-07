58fe1ab27e703f9fd47c4646a3b225b4dfc33840
dervin <dervin>
2009-04-07 19:33:34 +0000

    transfer query method constants

--- caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/DataServiceConstants.java
+++ caGrid/projects/data/src/java/common/gov/nih/nci/cagrid/data/DataServiceConstants.java
@@ -5,6 +5,8 @@ import gov.nih.nci.cagrid.encoding.SDKSerializerFactory;
 
 import javax.xml.namespace.QName;
 
+import org.cagrid.transfer.context.client.TransferServiceContextClient;
+
 
 /** 
  *  DataServiceConstants
@@ -136,6 +138,17 @@ public class DataServiceConstants {
     public static final QName BDT_HANDLER_REFERENCE_QNAME = new QName("http://cagrid.nci.nih.gov/BulkDataHandlerReference", "BulkDataHandlerReference");
     public static final QName BDT_QUERY_METHOD_INPUT_MESSAGE = new QName(BDT_DATA_SERVICE_NAMESPACE, "BdtQueryRequest");
     public static final QName BDT_QUERY_METHOD_OUTPUT_MESSAGE = new QName(BDT_DATA_SERVICE_NAMESPACE, "BdtQueryResponse");
+    
+    // transfer query method constants
+    public static final String TRANSFER_QUERY_METHOD_NAME = "transferQuery";
+    public static final String TRANSFER_QUERY_METHOD_DESCRIPTION = "The standard caGrid Data Service query method which returns results via the caGrid Transfer framework.";
+    public static final String TRANSFER_QUERY_METHOD_OUTPUT_DESCRIPTION = "The transfer context reference containing the EPR of the transfer resource";
+    public static final String TRANSFER_DATA_SERVICE_NAMESPACE = "http://gov.nih.nci.cagrid.data.transfer/TransferDataService";
+    public static final String TRANSFER_DATA_SERVICE_PACKAGE_NAME = DATA_SERVICE_PACKAGE + ".transfer";
+    public static final String TRANSFER_CONTEXT_CLIENT = TransferServiceContextClient.class.getName();
+    public static final QName TRANSFER_CONTEXT_REFERENCE_QNAME = new QName("http://transfer.cagrid.org/TransferService/Context/types", "TransferServiceContextReference");
+    public static final QName TRANSFER_QUERY_METHOD_INPUT_MESSAGE = new QName(TRANSFER_DATA_SERVICE_NAMESPACE, "TransferQueryRequest");
+    public static final QName TRANSFER_QUERY_METHOD_OUTPUT_MESSAGE = new QName(TRANSFER_DATA_SERVICE_NAMESPACE, "TransferQueryResponse");
     
     // auditor related constants
     public static final String DATA_SERVICE_AUDITING_NAMESPACE = "http://gov.nih.nci.cagrid.data/Auditing";
