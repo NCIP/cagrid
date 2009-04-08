7041233cce4a9165c175ec61fa50a1ea16b361c6
kherm <kherm>
2009-04-08 21:31:07 +0000

    commons-io

--- cagrid-1-0/Applications/cagrid-portal/portals/liferay/test/src/java/gov/nih/nci/cagrid/portal/liferay/websso/WebSSOUserDetailsServiceTest.java
+++ cagrid-1-0/Applications/cagrid-portal/portals/liferay/test/src/java/gov/nih/nci/cagrid/portal/liferay/websso/WebSSOUserDetailsServiceTest.java
@@ -1,6 +1,11 @@
 package gov.nih.nci.cagrid.portal.liferay.websso;
 
+import org.acegisecurity.userdetails.UserDetails;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 /**
  * User: kherm
@@ -11,9 +16,24 @@ public class WebSSOUserDetailsServiceTest {
 
 
     @Test
-    public void loadUserByUsername() {
+    public void loadUserByUsername() throws Exception {
         WebSSOUserDetailsService service = new WebSSOUserDetailsService();
-        service.loadUserByUsername(null);
+        try {
+            service.loadUserByUsername(null);
+            fail("Should not be able to parse null token");
+        } catch (Exception e) {
+            //expected
+        }
 
+        Resource resource = new FileSystemResource("liferay/test/data/webSSOToken.xml");
+        String token = FileUtils.readFileToString(resource.getFile());
+
+        UserDetails casUserId = service.loadUserByUsername(token);
+        assertNotNull(casUserId);
+        assertTrue(casUserId.getAuthorities().length > 0);
+        assertNotNull(casUserId.getUsername());
     }
+
+
 }
+
