e34852a256813cd44fa7da60bf3e6f416e79e2fc
kherm <kherm>
2009-04-17 18:39:11 +0000

    notification first cut

--- cagrid-1-0/Applications/cagrid-portal/aggr/src/java/gov/nih/nci/cagrid/portal/util/RunStatusChange.java
+++ cagrid-1-0/Applications/cagrid-portal/aggr/src/java/gov/nih/nci/cagrid/portal/util/RunStatusChange.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.util;
 
@@ -7,19 +7,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
  */
 public class RunStatusChange {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
 
-		new ClassPathXmlApplicationContext(new String[] {
-				"classpath:applicationContext-db.xml",
-				"classpath:applicationContext-aggr-status.xml" });
+        new ClassPathXmlApplicationContext(new String[]{
+                "classpath:applicationContext-db.xml",
+                "classpath:applicationContext-notification.xml",
+                "classpath:applicationContext-aggr-status.xml"});
 
-	}
+    }
 
 }
