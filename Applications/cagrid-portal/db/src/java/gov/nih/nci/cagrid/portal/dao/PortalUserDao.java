969b916c94eaa93676469f7328b277d21ffbc008
kherm <kherm>
2009-05-27 22:44:18 +0000

    catalogs

--- Applications/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/dao/PortalUserDao.java
+++ Applications/cagrid-portal/db/src/java/gov/nih/nci/cagrid/portal/dao/PortalUserDao.java
@@ -1,38 +1,41 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.dao;
 
 import gov.nih.nci.cagrid.portal.domain.PortalUser;
 
-import java.util.List;
-
 import javax.persistence.NonUniqueResultException;
+import java.util.List;
 
 /**
  * @author <a href="joshua.phillips@semanticbits.com">Joshua Phillips</a>
- *
  */
 public class PortalUserDao extends AbstractDao<PortalUser> {
 
-	/* (non-Javadoc)
-	 * @see gov.nih.nci.cagrid.portal.dao.AbstractDao#domainClass()
-	 */
-	@Override
-	public Class domainClass() {
-		return PortalUser.class;
-	}
+    /* (non-Javadoc)
+      * @see gov.nih.nci.cagrid.portal.dao.AbstractDao#domainClass()
+      */
+    @Override
+    public Class domainClass() {
+        return PortalUser.class;
+    }
 
-	public PortalUser getByPortalId(Long userId) {
-		PortalUser user = null;
-		List l = getHibernateTemplate().find("from PortalUser where portalId = '" + userId + "'");
-		if(l.size() > 1){
-			throw new NonUniqueResultException("More than one PortalUser found for portalId = " + userId);
-		}
-		if(l.size() == 1){
-			user = (PortalUser)l.iterator().next();
-		}
-		return user;
-	}
+    @Override
+    public void save(PortalUser domainObject) {
+        super.save(domainObject);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public PortalUser getByPortalId(Long userId) {
+        PortalUser user = null;
+        List l = getHibernateTemplate().find("from PortalUser where portalId = '" + userId + "'");
+        if (l.size() > 1) {
+            throw new NonUniqueResultException("More than one PortalUser found for portalId = " + userId);
+        }
+        if (l.size() == 1) {
+            user = (PortalUser) l.iterator().next();
+        }
+        return user;
+    }
 
 }
