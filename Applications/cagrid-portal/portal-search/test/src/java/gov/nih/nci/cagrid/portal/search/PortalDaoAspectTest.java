245089b0b9dff2e44d3cef838b4e0d97bf1bb465
kherm <kherm>
2009-07-02 18:04:53 +0000

    browse tree

---
+++ Applications/cagrid-portal/portal-search/test/src/java/gov/nih/nci/cagrid/portal/search/PortalDaoAspectTest.java
@@ -0,0 +1,129 @@
+package gov.nih.nci.cagrid.portal.search;
+
+import gov.nih.nci.cagrid.portal.DBIntegrationTestBase;
+import gov.nih.nci.cagrid.portal.dao.PortalUserDao;
+import gov.nih.nci.cagrid.portal.dao.catalog.CatalogEntryDao;
+import gov.nih.nci.cagrid.portal.dao.catalog.GridServiceEndPointCatalogEntryDao;
+import gov.nih.nci.cagrid.portal.dao.catalog.InstitutionCatalogEntryDao;
+import gov.nih.nci.cagrid.portal.dao.catalog.PersonCatalogEntryDao;
+import gov.nih.nci.cagrid.portal.domain.catalog.CatalogEntry;
+import gov.nih.nci.cagrid.portal.domain.catalog.GridServiceEndPointCatalogEntry;
+import gov.nih.nci.cagrid.portal.domain.catalog.InstitutionCatalogEntry;
+import gov.nih.nci.cagrid.portal.domain.catalog.PersonCatalogEntry;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+/**
+ * User: kherm
+ *
+ * @author kherm manav.kher@semanticbits.com
+ */
+public class PortalDaoAspectTest extends DBIntegrationTestBase {
+
+
+    PersonCatalogEntryDao personCatalogEntryDao;
+    PortalUserDao portalUserDao;
+
+    @Override
+    protected ConfigurableApplicationContext loadContext(Object o) throws Exception {
+        GenericApplicationContext ctx = new GenericApplicationContext();
+
+        XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ctx);
+        xmlReader.loadBeanDefinitions(new ClassPathResource("applicationContext-db.xml"));
+        xmlReader.loadBeanDefinitions(new ClassPathResource("applicationContext-portal-search.xml"));
+
+        ctx.registerBeanDefinition("defaultHttpClient", new RootBeanDefinition(MockHttpClient.class));
+        ctx.refresh();
+
+
+        return ctx;
+    }
+
+
+    public void testPersonCEAspect() {
+        PersonCatalogEntry pCE = new PersonCatalogEntry();
+        PersonCatalogEntryDao pCEDao = (PersonCatalogEntryDao) getApplicationContext().getBean("personCatalogEntryDao");
+        pCEDao.save(pCE);
+
+    }
+
+    public void testCEAspect() {
+        CatalogEntry ce = new CatalogEntry();
+        CatalogEntryDao cEDao = (CatalogEntryDao) getApplicationContext().getBean("catalogEntryDao");
+        cEDao.save(ce);
+
+    }
+
+    public void testGSCEAspect() {
+        GridServiceEndPointCatalogEntry ce = new GridServiceEndPointCatalogEntry();
+        GridServiceEndPointCatalogEntryDao cEDao = (GridServiceEndPointCatalogEntryDao) getApplicationContext().getBean("gridServiceEndPointCatalogEntryDao");
+        cEDao.save(ce);
+
+    }
+
+    public void testInstCEAspect() {
+        InstitutionCatalogEntry ce = new InstitutionCatalogEntry();
+        InstitutionCatalogEntryDao cEDao = (InstitutionCatalogEntryDao) getApplicationContext().getBean("institutionCatalogEntryDao");
+        cEDao.save(ce);
+
+    }
+
+
+    @Override
+    protected void onTearDown() throws Exception {
+        super.onTearDown();    //To change body of overridden methods use File | Settings | File Templates.
+        assertTrue("Solr HTTP interface was not called", MockHttpClient.assertJustRan());
+    }
+
+    public PersonCatalogEntryDao getPersonCatalogEntryDao() {
+        return personCatalogEntryDao;
+    }
+
+    public void setPersonCatalogEntryDao(PersonCatalogEntryDao personCatalogEntryDao) {
+        this.personCatalogEntryDao = personCatalogEntryDao;
+    }
+
+    public PortalUserDao getPortalUserDao() {
+        return portalUserDao;
+    }
+
+    public void setPortalUserDao(PortalUserDao portalUserDao) {
+        this.portalUserDao = portalUserDao;
+    }
+
+
+    static class MockHttpClient extends HttpClient {
+
+        private static boolean assertRan = false;
+
+        public MockHttpClient() {
+        }
+
+
+        @Override
+        public int executeMethod(HttpMethod httpMethod) throws IOException, HttpException {
+            assertRan = true;
+            return 0;
+        }
+
+        public static boolean assertJustRan() {
+            if (assertRan) {
+                assertRan = false;
+                return !assertRan;
+            }
+            return assertRan;
+
+        }
+    }
+
+}
+
+
