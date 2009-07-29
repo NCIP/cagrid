c71c162e81a8d4bf917a24369c9188cf31d44c55
cmelean <cmelean>
2009-07-29 18:12:03 +0000

    identifiers - load naming authority project

---
+++ caGrid/projects/identifiers-namingauthority/src/org/cagrid/identifiers/namingauthority/NamingAuthorityConfig.java
@@ -0,0 +1,11 @@
+package org.cagrid.identifiers.namingauthority;
+
+public interface NamingAuthorityConfig {
+	
+	public String getPrefix();
+	public Integer getHttpServerPort();
+	public String getGridSvcUrl();
+	public String getDbUserName();
+	public String getDbPassword();
+	public String getDbUrl();
+}
