8d62ef498ea36f08262a9a6ff8bc6a569175b0f8
kherm <kherm>
2009-08-14 15:26:32 +0000

    fix  CAGPRTL-26

--- Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/query/results/ExportJSONResultsMetadataController.java
+++ Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/query/results/ExportJSONResultsMetadataController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.query.results;
 
@@ -7,67 +7,74 @@ import gov.nih.nci.cagrid.portal.dao.QueryResultTableDao;
 import gov.nih.nci.cagrid.portal.domain.dataservice.QueryInstance;
 import gov.nih.nci.cagrid.portal.domain.table.QueryResultTable;
 import gov.nih.nci.cagrid.portal.portlet.UserModel;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.json.JSONObject;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
+ * @author <a href="mailto:manav.kher@semanticbits.com">Manav Kher</a>
  */
 public class ExportJSONResultsMetadataController extends AbstractController {
 
-	private UserModel userModel;
-	private QueryResultTableDao queryResultTableDao;
+    private UserModel userModel;
+    private QueryResultTableDao queryResultTableDao;
+    private QueryResultTableToDataTableMetadataBuilder queryResultTableToDataTableMetadataBuilder;
 
-	/**
-	 * 
-	 */
-	public ExportJSONResultsMetadataController() {
+    /**
+     *
+     */
+    public ExportJSONResultsMetadataController() {
 
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.web.servlet.mvc.AbstractController#handleRequestInternal
-	 * (javax.servlet.http.HttpServletRequest,
-	 * javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest req,
-			HttpServletResponse res) throws Exception {
+    /*
+      * (non-Javadoc)
+      *
+      * @see
+      * org.springframework.web.servlet.mvc.AbstractController#handleRequestInternal
+      * (javax.servlet.http.HttpServletRequest,
+      * javax.servlet.http.HttpServletResponse)
+      */
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest req,
+                                                 HttpServletResponse res) throws Exception {
 
-		QueryInstance instance = getUserModel().getCurrentQueryInstance();
-		QueryResultTable table = getQueryResultTableDao().getByQueryInstanceId(
-				instance.getId());
-		JSONObject meta = new QueryResultTableToDataTableMetadataBuilder()
-				.build(table);
-		res.setContentType("application/json");
-		res.getWriter().write(meta.toString());
+        QueryInstance instance = getUserModel().getCurrentQueryInstance();
+        QueryResultTable table = getQueryResultTableDao().getByQueryInstanceId(
+                instance.getId());
+        JSONObject meta = queryResultTableToDataTableMetadataBuilder
+                .build(table);
+        res.setContentType("application/json");
+        res.getWriter().write(meta.toString());
 
-		return null;
-	}
+        return null;
+    }
 
-	public UserModel getUserModel() {
-		return userModel;
-	}
+    public UserModel getUserModel() {
+        return userModel;
+    }
 
-	public void setUserModel(UserModel userModel) {
-		this.userModel = userModel;
-	}
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
+    }
 
-	public QueryResultTableDao getQueryResultTableDao() {
-		return queryResultTableDao;
-	}
+    public QueryResultTableDao getQueryResultTableDao() {
+        return queryResultTableDao;
+    }
 
-	public void setQueryResultTableDao(QueryResultTableDao queryResultTableDao) {
-		this.queryResultTableDao = queryResultTableDao;
-	}
+    public void setQueryResultTableDao(QueryResultTableDao queryResultTableDao) {
+        this.queryResultTableDao = queryResultTableDao;
+    }
 
+    public QueryResultTableToDataTableMetadataBuilder getQueryResultTableToDataTableMetadataBuilder() {
+        return queryResultTableToDataTableMetadataBuilder;
+    }
+
+    public void setQueryResultTableToDataTableMetadataBuilder(QueryResultTableToDataTableMetadataBuilder queryResultTableToDataTableMetadataBuilder) {
+        this.queryResultTableToDataTableMetadataBuilder = queryResultTableToDataTableMetadataBuilder;
+    }
 }
--- Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/query/results/QueryResultTableToDataTableMetadataBuilder.java
+++ Applications/cagrid-portal/portlets/src/java/gov/nih/nci/cagrid/portal/portlet/query/results/QueryResultTableToDataTableMetadataBuilder.java
@@ -1,105 +1,118 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.query.results;
 
+import gov.nih.nci.cagrid.portal.dao.QueryResultTableDao;
 import gov.nih.nci.cagrid.portal.domain.table.QueryResultColumn;
 import gov.nih.nci.cagrid.portal.domain.table.QueryResultTable;
 import gov.nih.nci.cagrid.portal.portlet.util.PortletUtils;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.JSONObject;
+
+import java.util.*;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
+ * @author <a href="mailto:manav.kher@semanticbits.com">Manav Kher</a>
  */
 public class QueryResultTableToDataTableMetadataBuilder {
 
-	private Map<String, String> columnTypeFormatterMap;
+    private Map<String, String> columnTypeFormatterMap;
+    private QueryResultTableDao queryResultTableDao;
+    private Log log = LogFactory.getLog(QueryResultTableToDataTableMetadataBuilder.class);
 
-	public JSONObject build(QueryResultTable table) {
-		try {
-			JSONObject meta = new JSONObject();
+    public JSONObject build(QueryResultTable table) {
+        try {
+            JSONObject meta = new JSONObject();
 
-			Map<String, String> formatters = new HashMap<String, String>();
-			List<String> allColNames = getOrderedColumnNames(table);
+            Map<String, String> formatters = new HashMap<String, String>();
+            List<String> allColNames = getOrderedColumnNames(table);
 
-			for (String colName : allColNames) {
-				JSONObject colDef = new JSONObject();
-				colDef.put("key", colName);
-				colDef.put("sortable", true);
-				colDef.put("resizeable", true);
-				String formatter = formatters.get(colName);
-				if (formatter != null) {
-					colDef.put("formatter", formatter);
-				}
-				meta.append("columnDefs", colDef);
-			}
+            for (String colName : allColNames) {
+                JSONObject colDef = new JSONObject();
+                colDef.put("key", colName);
+                boolean sortable = false;
+                try {
+                    sortable = queryResultTableDao.getRowCountForColumn(table.getId(), colName) > 0;
+                } catch (Exception e) {
+                    // lets not break the UI because of this
+                    log.error("error getting row count for column " + colName, e);
+                }
+                colDef.put("sortable", sortable);
+                colDef.put("resizeable", true);
+                String formatter = formatters.get(colName);
+                if (formatter != null) {
+                    colDef.put("formatter", formatter);
+                }
+                meta.append("columnDefs", colDef);
+            }
 
-			JSONObject responseSchema = new JSONObject();
-			meta.put("responseSchema", responseSchema);
-			responseSchema.put("resultsList", "rows");
-			JSONObject metaFields = new JSONObject();
-			responseSchema.put("metaFields", metaFields);
-			metaFields.put("totalRecords", "numRows");
+            JSONObject responseSchema = new JSONObject();
+            meta.put("responseSchema", responseSchema);
+            responseSchema.put("resultsList", "rows");
+            JSONObject metaFields = new JSONObject();
+            responseSchema.put("metaFields", metaFields);
+            metaFields.put("totalRecords", "numRows");
 
-			for (String colName : allColNames) {
-				responseSchema.append("fields", colName);
-			}
+            for (String colName : allColNames) {
+                responseSchema.append("fields", colName);
+            }
 
-			return meta;
-		} catch (Exception ex) {
-			throw new RuntimeException("Error building metadata: "
-					+ ex.getMessage(), ex);
-		}
-	}
-	
-	public static List<String> getOrderedColumnNames(QueryResultTable table){
-		List<String> allColNames = new ArrayList<String>();
+            return meta;
+        } catch (Exception ex) {
+            throw new RuntimeException("Error building metadata: "
+                    + ex.getMessage(), ex);
+        }
+    }
 
-		if (PortletUtils.isCountQuery(table.getQueryInstance().getQuery()
-				.getXml())) {
-			allColNames.add("count");
-		} else {
-			SortedSet<String> sortedColNames = new TreeSet<String>();
-			for (QueryResultColumn col : table.getColumns()) {
-				sortedColNames.add(col.getName());
-			}
-			if (sortedColNames.contains("id")) {
-				allColNames.add("id");
-			}
-			for (String colName : sortedColNames) {
-				if (!"id".equals(colName)) {
-					allColNames.add(colName);
-				}
-			}
-		}
-		allColNames.add(ResultConstants.DATA_SERVICE_URL_COL_NAME);
-		return allColNames;
-	}
+    public static List<String> getOrderedColumnNames(QueryResultTable table) {
+        List<String> allColNames = new ArrayList<String>();
 
-	protected String getFormatter(QueryResultColumn column) {
-		String formatter = null;
-		if (getColumnTypeFormatterMap() != null) {
-			formatter = getColumnTypeFormatterMap().get(column.getName());
-		}
-		return formatter;
-	}
+        if (PortletUtils.isCountQuery(table.getQueryInstance().getQuery()
+                .getXml())) {
+            allColNames.add("count");
+        } else {
+            SortedSet<String> sortedColNames = new TreeSet<String>();
+            for (QueryResultColumn col : table.getColumns()) {
+                sortedColNames.add(col.getName());
+            }
+            if (sortedColNames.contains("id")) {
+                allColNames.add("id");
+            }
+            for (String colName : sortedColNames) {
+                if (!"id".equals(colName)) {
+                    allColNames.add(colName);
+                }
+            }
+        }
+        allColNames.add(ResultConstants.DATA_SERVICE_URL_COL_NAME);
+        return allColNames;
+    }
 
-	public Map<String, String> getColumnTypeFormatterMap() {
-		return columnTypeFormatterMap;
-	}
+    protected String getFormatter(QueryResultColumn column) {
+        String formatter = null;
+        if (getColumnTypeFormatterMap() != null) {
+            formatter = getColumnTypeFormatterMap().get(column.getName());
+        }
+        return formatter;
+    }
 
-	public void setColumnTypeFormatterMap(
-			Map<String, String> columnTypeFormatterMap) {
-		this.columnTypeFormatterMap = columnTypeFormatterMap;
-	}
+    public Map<String, String> getColumnTypeFormatterMap() {
+        return columnTypeFormatterMap;
+    }
 
+    public void setColumnTypeFormatterMap(
+            Map<String, String> columnTypeFormatterMap) {
+        this.columnTypeFormatterMap = columnTypeFormatterMap;
+    }
+
+    public QueryResultTableDao getQueryResultTableDao() {
+        return queryResultTableDao;
+    }
+
+    public void setQueryResultTableDao(QueryResultTableDao queryResultTableDao) {
+        this.queryResultTableDao = queryResultTableDao;
+    }
 }
