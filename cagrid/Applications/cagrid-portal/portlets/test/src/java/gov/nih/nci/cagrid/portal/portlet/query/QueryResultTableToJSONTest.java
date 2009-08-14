be1d5c3acdb1e3df72f4b83fc5f2a3891538b933
kherm <kherm>
2009-08-14 17:51:06 +0000

    fix test

--- cagrid/Applications/cagrid-portal/portlets/test/src/java/gov/nih/nci/cagrid/portal/portlet/query/QueryResultTableToJSONTest.java
+++ cagrid/Applications/cagrid-portal/portlets/test/src/java/gov/nih/nci/cagrid/portal/portlet/query/QueryResultTableToJSONTest.java
@@ -1,11 +1,9 @@
 /**
- * 
+ *
  */
 package gov.nih.nci.cagrid.portal.portlet.query;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import gov.nih.nci.cagrid.portal.dao.QueryResultTableDao;
 import gov.nih.nci.cagrid.portal.domain.dataservice.CQLQuery;
 import gov.nih.nci.cagrid.portal.domain.dataservice.CQLQueryInstance;
 import gov.nih.nci.cagrid.portal.domain.table.QueryResultTable;
@@ -13,188 +11,202 @@ import gov.nih.nci.cagrid.portal.portlet.query.results.QueryResultTableToDataTab
 import gov.nih.nci.cagrid.portal.portlet.query.results.QueryResultTableToJSONObjectBuilder;
 import gov.nih.nci.cagrid.portal.portlet.query.results.XMLQueryResultToQueryResultTableHandler;
 import gov.nih.nci.cagrid.portal.portlet.util.PortletUtils;
+import org.json.JSONObject;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.json.JSONObject;
-import org.junit.Test;
-
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com">Joshua Phillips</a>
- * 
+ * @author <a href="mailto:manav.kher@semanticbits.com">Manav Kher</a>
  */
 public class QueryResultTableToJSONTest {
 
-	/**
-	 * 
-	 */
-	public QueryResultTableToJSONTest() {
+    /**
+     *
+     */
 
-	}
+    QueryResultTableToDataTableMetadataBuilder builder;
 
-	@Test
-	public void testCountQueryJSONMeta() {
-		try {
+    public QueryResultTableToJSONTest() {
 
-			StringBuilder sb = new StringBuilder();
-			BufferedReader r = new BufferedReader(new FileReader(
-					"test/data/count_query.xml"));
-			String line = null;
-			while ((line = r.readLine()) != null) {
-				sb.append(line);
-			}
-			String cql = sb.toString();
-			assertTrue(PortletUtils.isCountQuery(cql));
 
-			CQLQuery query = new CQLQuery();
-			query.setXml(cql);
-			CQLQueryInstance queryInstance = new CQLQueryInstance();
-			queryInstance.setResult("some data");
-			queryInstance.setQuery(query);
-			XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
-			handler.setPersist(false);
-			handler.setDataServiceUrl("http://service");
-			handler.getTable().setQueryInstance(queryInstance);
-			SAXParserFactory fact = SAXParserFactory.newInstance();
-			fact.setNamespaceAware(true);
-			SAXParser parser = fact.newSAXParser();
-			parser.parse(new FileInputStream("test/data/count_results.xml"),
-					handler);
-			QueryResultTable table = handler.getTable();
-			QueryResultTableToDataTableMetadataBuilder builder = new QueryResultTableToDataTableMetadataBuilder();
-			String expected = "{\"responseSchema\":{\"resultsList\":\"rows\",\"fields\":[\"count\",\"dataServiceUrl\"],\"metaFields\":{\"totalRecords\":\"numRows\"}},\"columnDefs\":[{\"key\":\"count\",\"resizeable\":true,\"sortable\":true},{\"key\":\"dataServiceUrl\",\"resizeable\":true,\"sortable\":true}]}";
-			assertEquals(expected, builder.build(table).toString());
+    }
 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			String msg = "Error encountered: " + ex.getMessage();
-			fail(msg);
-		}
-	}
-	
-	@Test
-	public void testCountQueryJSON() {
-		try {
 
-			StringBuilder sb = new StringBuilder();
-			BufferedReader r = new BufferedReader(new FileReader(
-					"test/data/count_query.xml"));
-			String line = null;
-			while ((line = r.readLine()) != null) {
-				sb.append(line);
-			}
-			String cql = sb.toString();
-			assertTrue(PortletUtils.isCountQuery(cql));
+    @Before
+    public void setup() {
+        builder = new QueryResultTableToDataTableMetadataBuilder();
+        QueryResultTableDao mockResultTableDao = mock(QueryResultTableDao.class);
+        when(mockResultTableDao.getRowCountForColumn(anyInt(), anyString())).thenReturn(2);
 
-			CQLQuery query = new CQLQuery();
-			query.setXml(cql);
-			CQLQueryInstance queryInstance = new CQLQueryInstance();
-			queryInstance.setResult("some data");
-			queryInstance.setQuery(query);
-			XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
-			handler.setDataServiceUrl("http://service");
-			handler.setPersist(false);
-			handler.getTable().setQueryInstance(queryInstance);
-			SAXParserFactory fact = SAXParserFactory.newInstance();
-			fact.setNamespaceAware(true);
-			SAXParser parser = fact.newSAXParser();
-			parser.parse(new FileInputStream("test/data/count_results.xml"),
-					handler);
-			QueryResultTable table = handler.getTable();
-			QueryResultTableToJSONObjectBuilder builder = new QueryResultTableToJSONObjectBuilder();
-			String expected = "{\"numRows\":1,\"rows\":[{\"dataServiceUrl\":\"http://service\",\"count\":\"1208\"}]}";
-			assertEquals(expected, builder.build(table.getRows()).toString());
+        builder.setQueryResultTableDao(mockResultTableDao);
 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			String msg = "Error encountered: " + ex.getMessage();
-			fail(msg);
-		}
-	}
-	
-	@Test
-	public void testSelectedAttributesQueryJSONMeta() {
-		try {
-			StringBuilder sb = new StringBuilder();
-			BufferedReader r = new BufferedReader(new FileReader(
-					"test/data/selected_attribute_query.xml"));
-			String line = null;
-			while ((line = r.readLine()) != null) {
-				sb.append(line);
-			}
-			String cql = sb.toString();
-			assertTrue(!PortletUtils.isCountQuery(cql));
+    }
 
-			CQLQuery query = new CQLQuery();
-			query.setXml(cql);
-			CQLQueryInstance queryInstance = new CQLQueryInstance();
-			queryInstance.setResult("some data");
-			queryInstance.setQuery(query);
-			XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
-			handler.setPersist(false);
-			handler.setDataServiceUrl("http://service");
-			handler.getTable().setQueryInstance(queryInstance);
-			SAXParserFactory fact = SAXParserFactory.newInstance();
-			fact.setNamespaceAware(true);
-			SAXParser parser = fact.newSAXParser();
-			parser.parse(new FileInputStream(
-					"test/data/selected_attribute_results.xml"), handler);
-			QueryResultTable table = handler.getTable();
-			
-			QueryResultTableToDataTableMetadataBuilder builder = new QueryResultTableToDataTableMetadataBuilder();
-			String expected ="{\"responseSchema\":{\"resultsList\":\"rows\",\"fields\":[\"id\",\"ageOfOnset\",\"dataServiceUrl\"],\"metaFields\":{\"totalRecords\":\"numRows\"}},\"columnDefs\":[{\"key\":\"id\",\"resizeable\":true,\"sortable\":true},{\"key\":\"ageOfOnset\",\"resizeable\":true,\"sortable\":true},{\"key\":\"dataServiceUrl\",\"resizeable\":true,\"sortable\":true}]}";
-			assertEquals(expected, builder.build(table).toString());
+    @Test
+    public void testCountQueryJSONMeta() {
+        try {
 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			String msg = "Error encountered: " + ex.getMessage();
-			fail(msg);
-		}
-	}
-	
-	@Test
-	public void testSelectedAttributesQueryJSON() {
-		try {
-			StringBuilder sb = new StringBuilder();
-			BufferedReader r = new BufferedReader(new FileReader(
-					"test/data/selected_attribute_query.xml"));
-			String line = null;
-			while ((line = r.readLine()) != null) {
-				sb.append(line);
-			}
-			String cql = sb.toString();
-			assertTrue(!PortletUtils.isCountQuery(cql));
+            StringBuilder sb = new StringBuilder();
+            BufferedReader r = new BufferedReader(new FileReader(
+                    "test/data/count_query.xml"));
+            String line = null;
+            while ((line = r.readLine()) != null) {
+                sb.append(line);
+            }
+            String cql = sb.toString();
+            assertTrue(PortletUtils.isCountQuery(cql));
 
-			CQLQuery query = new CQLQuery();
-			query.setXml(cql);
-			CQLQueryInstance queryInstance = new CQLQueryInstance();
-			queryInstance.setResult("some data");
-			queryInstance.setQuery(query);
-			XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
-			handler.setPersist(false);
-			handler.setDataServiceUrl("http://service");
-			handler.getTable().setQueryInstance(queryInstance);
-			SAXParserFactory fact = SAXParserFactory.newInstance();
-			fact.setNamespaceAware(true);
-			SAXParser parser = fact.newSAXParser();
-			parser.parse(new FileInputStream(
-					"test/data/selected_attribute_results.xml"), handler);
-			QueryResultTable table = handler.getTable();
-			
-			QueryResultTableToJSONObjectBuilder builder = new QueryResultTableToJSONObjectBuilder();
-			JSONObject out = builder.build(table.getRows());
-			assertEquals(1000, out.get("numRows"));
+            CQLQuery query = new CQLQuery();
+            query.setXml(cql);
+            CQLQueryInstance queryInstance = new CQLQueryInstance();
+            queryInstance.setResult("some data");
+            queryInstance.setQuery(query);
+            XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
+            handler.setPersist(false);
+            handler.setDataServiceUrl("http://service");
+            handler.getTable().setQueryInstance(queryInstance);
+            SAXParserFactory fact = SAXParserFactory.newInstance();
+            fact.setNamespaceAware(true);
+            SAXParser parser = fact.newSAXParser();
+            parser.parse(new FileInputStream("test/data/count_results.xml"),
+                    handler);
+            QueryResultTable table = handler.getTable();
+            String expected = "{\"responseSchema\":{\"resultsList\":\"rows\",\"fields\":[\"count\",\"dataServiceUrl\"],\"metaFields\":{\"totalRecords\":\"numRows\"}},\"columnDefs\":[{\"key\":\"count\",\"resizeable\":true,\"sortable\":true},{\"key\":\"dataServiceUrl\",\"resizeable\":true,\"sortable\":true}]}";
+            assertEquals(expected, builder.build(table).toString());
 
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			String msg = "Error encountered: " + ex.getMessage();
-			fail(msg);
-		}
-	}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            String msg = "Error encountered: " + ex.getMessage();
+            fail(msg);
+        }
+    }
+
+    @Test
+    public void testCountQueryJSON() {
+        try {
+
+            StringBuilder sb = new StringBuilder();
+            BufferedReader r = new BufferedReader(new FileReader(
+                    "test/data/count_query.xml"));
+            String line = null;
+            while ((line = r.readLine()) != null) {
+                sb.append(line);
+            }
+            String cql = sb.toString();
+            assertTrue(PortletUtils.isCountQuery(cql));
+
+            CQLQuery query = new CQLQuery();
+            query.setXml(cql);
+            CQLQueryInstance queryInstance = new CQLQueryInstance();
+            queryInstance.setResult("some data");
+            queryInstance.setQuery(query);
+            XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
+            handler.setDataServiceUrl("http://service");
+            handler.setPersist(false);
+            handler.getTable().setQueryInstance(queryInstance);
+            SAXParserFactory fact = SAXParserFactory.newInstance();
+            fact.setNamespaceAware(true);
+            SAXParser parser = fact.newSAXParser();
+            parser.parse(new FileInputStream("test/data/count_results.xml"),
+                    handler);
+            QueryResultTable table = handler.getTable();
+            QueryResultTableToJSONObjectBuilder builder = new QueryResultTableToJSONObjectBuilder();
+            String expected = "{\"numRows\":1,\"rows\":[{\"dataServiceUrl\":\"http://service\",\"count\":\"1208\"}]}";
+            assertEquals(expected, builder.build(table.getRows()).toString());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            String msg = "Error encountered: " + ex.getMessage();
+            fail(msg);
+        }
+    }
+
+    @Test
+    public void testSelectedAttributesQueryJSONMeta() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            BufferedReader r = new BufferedReader(new FileReader(
+                    "test/data/selected_attribute_query.xml"));
+            String line = null;
+            while ((line = r.readLine()) != null) {
+                sb.append(line);
+            }
+            String cql = sb.toString();
+            assertTrue(!PortletUtils.isCountQuery(cql));
+
+            CQLQuery query = new CQLQuery();
+            query.setXml(cql);
+            CQLQueryInstance queryInstance = new CQLQueryInstance();
+            queryInstance.setResult("some data");
+            queryInstance.setQuery(query);
+            XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
+            handler.setPersist(false);
+            handler.setDataServiceUrl("http://service");
+            handler.getTable().setQueryInstance(queryInstance);
+            SAXParserFactory fact = SAXParserFactory.newInstance();
+            fact.setNamespaceAware(true);
+            SAXParser parser = fact.newSAXParser();
+            parser.parse(new FileInputStream(
+                    "test/data/selected_attribute_results.xml"), handler);
+            QueryResultTable table = handler.getTable();
+
+            String expected = "{\"responseSchema\":{\"resultsList\":\"rows\",\"fields\":[\"id\",\"ageOfOnset\",\"dataServiceUrl\"],\"metaFields\":{\"totalRecords\":\"numRows\"}},\"columnDefs\":[{\"key\":\"id\",\"resizeable\":true,\"sortable\":true},{\"key\":\"ageOfOnset\",\"resizeable\":true,\"sortable\":true},{\"key\":\"dataServiceUrl\",\"resizeable\":true,\"sortable\":true}]}";
+            assertEquals(expected, builder.build(table).toString());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            String msg = "Error encountered: " + ex.getMessage();
+            fail(msg);
+        }
+    }
+
+    @Test
+    public void testSelectedAttributesQueryJSON() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            BufferedReader r = new BufferedReader(new FileReader(
+                    "test/data/selected_attribute_query.xml"));
+            String line = null;
+            while ((line = r.readLine()) != null) {
+                sb.append(line);
+            }
+            String cql = sb.toString();
+            assertTrue(!PortletUtils.isCountQuery(cql));
+
+            CQLQuery query = new CQLQuery();
+            query.setXml(cql);
+            CQLQueryInstance queryInstance = new CQLQueryInstance();
+            queryInstance.setResult("some data");
+            queryInstance.setQuery(query);
+            XMLQueryResultToQueryResultTableHandler handler = new XMLQueryResultToQueryResultTableHandler();
+            handler.setPersist(false);
+            handler.setDataServiceUrl("http://service");
+            handler.getTable().setQueryInstance(queryInstance);
+            SAXParserFactory fact = SAXParserFactory.newInstance();
+            fact.setNamespaceAware(true);
+            SAXParser parser = fact.newSAXParser();
+            parser.parse(new FileInputStream(
+                    "test/data/selected_attribute_results.xml"), handler);
+            QueryResultTable table = handler.getTable();
+
+            QueryResultTableToJSONObjectBuilder builder = new QueryResultTableToJSONObjectBuilder();
+            JSONObject out = builder.build(table.getRows());
+            assertEquals(1000, out.get("numRows"));
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            String msg = "Error encountered: " + ex.getMessage();
+            fail(msg);
+        }
+    }
 
 }
