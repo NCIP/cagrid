c0c6bffe83defcb43e3cf3a3e4c4fc9238ed425b
osters <osters>
2009-09-18 15:40:30 +0000

    fixed up generics... casts no longer necessary on deserialization

--- caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/ConfigurableObjectDeserializer.java
+++ caGrid/projects/core/src/java/gov/nih/nci/cagrid/common/ConfigurableObjectDeserializer.java
@@ -11,30 +11,28 @@ import org.globus.wsrf.encoding.ObjectDeserializer;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-/** 
- *  ConfigurableObjectDeserializer
- *  Object Deserializer that allows for configuration by a wsdd file
+
+/**
+ * ConfigurableObjectDeserializer Object Deserializer that allows for
+ * configuration by a wsdd file
  * 
  * @author <A HREF="MAILTO:ervin@bmi.osu.edu">David W. Ervin</A>
- * 
- * @created Jun 23, 2006 
- * @version $Id$ 
+ * @created Jun 23, 2006
+ * @version $Id$
  */
 public class ConfigurableObjectDeserializer extends ObjectDeserializer {
-	
-	public static Object toObject(InputSource source, Class javaClass, InputStream wsdd)
-	    throws SAXException {
-		// create a message context for the wsdd
-		EngineConfiguration engineConfig = new FileProvider(wsdd);
-		AxisClient axisClient = new AxisClient(engineConfig);
-		MessageContext messageContext = new MessageContext(axisClient);
-		messageContext.setEncodingStyle("");
-		messageContext.setProperty(AxisEngine.PROP_DOMULTIREFS, Boolean.FALSE);
-		
-		ConfigurableObjectDeserializationContext desContext 
-			= new ConfigurableObjectDeserializationContext(
-				messageContext, source, javaClass);
-		
-		return desContext.getValue();
-	}
+
+    public static <T> T toObject(InputSource source, Class<T> javaClass, InputStream wsdd) throws SAXException {
+        // create a message context for the wsdd
+        EngineConfiguration engineConfig = new FileProvider(wsdd);
+        AxisClient axisClient = new AxisClient(engineConfig);
+        MessageContext messageContext = new MessageContext(axisClient);
+        messageContext.setEncodingStyle("");
+        messageContext.setProperty(AxisEngine.PROP_DOMULTIREFS, Boolean.FALSE);
+
+        ConfigurableObjectDeserializationContext desContext = new ConfigurableObjectDeserializationContext(
+            messageContext, source, javaClass);
+
+        return javaClass.cast(desContext.getValue());
+    }
 }
