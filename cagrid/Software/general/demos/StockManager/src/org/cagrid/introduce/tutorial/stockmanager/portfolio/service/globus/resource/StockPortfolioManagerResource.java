c5dd94d9ce8dc077426f72e5aeb238620bbe62de
jgeorge <jgeorge>
2010-01-26 21:29:30 +0000

    Version 1.3 of the Advanced caGrid Analytical Service

---
+++ cagrid/Software/general/demos/StockManager/src/org/cagrid/introduce/tutorial/stockmanager/portfolio/service/globus/resource/StockPortfolioManagerResource.java
@@ -0,0 +1,17 @@
+package org.cagrid.introduce.tutorial.stockmanager.portfolio.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this StockPortfolioManagerResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class StockPortfolioManagerResource extends StockPortfolioManagerResourceBase {
+
+}
---
+++ cagrid/Software/general/demos/StockManager/src/org/cagrid/introduce/tutorial/stockmanager/service/globus/resource/StockManagerResource.java
@@ -0,0 +1,17 @@
+package org.cagrid.introduce.tutorial.stockmanager.service.globus.resource;
+
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceKey;
+
+
+/** 
+ * The implementation of this StockManagerResource type.
+ * 
+ * @created by Introduce Toolkit version 1.3
+ * 
+ */
+public class StockManagerResource extends StockManagerResourceBase {
+
+}
